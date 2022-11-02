aadc4b98f7a9cb9a10f2818b004142995190b697
Elijah <ewbrown1@widener.edu>
2022-11-02 16:40:23 -0400

    added other pages

---
+++ app/src/main/java/com/example/haircareapp/ChatbotPage.java
@@ -0,0 +1,14 @@
+package com.example.haircareapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatbotPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatbot_page);
+    }
+}
