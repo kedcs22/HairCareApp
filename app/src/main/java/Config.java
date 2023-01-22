4d211b6cd75738bf9699e8be1acf7acb66d18445
Elijah <ewbrown1@widener.edu>
2023-01-22 14:37:29 -0500

    tapping into YT API

---
+++ app/src/main/java/Config.java
@@ -0,0 +1,13 @@
+//package com.echessa.videotube;
+
+/**
+ * Created by echessa on 1/13/17.
+ */
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyDYtzNF6N_4N2HK5efkC0pLxw4eiZS8was";
+
+}
