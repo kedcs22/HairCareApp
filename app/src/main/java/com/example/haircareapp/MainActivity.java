a66bed8c90abf59f14e44bbec120293458d8578d
Elijah <ewbrown1@widener.edu>
2023-02-22 15:58:38 -0500

    populate yt videos on main activity

--- app/src/main/java/com/example/haircareapp/MainActivity.java
+++ app/src/main/java/com/example/haircareapp/MainActivity.java
@@ -8,6 +8,8 @@ import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
@@ -17,13 +19,14 @@ import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends YouTubeBaseActivity {
 
     String api_key = "AIzaSyDYtzNF6N_4N2HK5efkC0pLxw4eiZS8was";
 
     private ImageButton homePage;
     private ImageButton chatBot;
     private ImageButton profilePage;
+    private Button PlayVideo1;
 
 
     @Override
@@ -32,10 +35,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Get reference to the view of Video player
-        YouTubePlayerView ytPlayer = (YouTubePlayerView)findViewById(R.id.youtube_view);
 
-        ytPlayer.initialize(
-                api_key,
+        homePage = findViewById(R.id.homePage);
+        profilePage = findViewById(R.id.profilePage);
+        chatBot = findViewById(R.id.chatBot);
+        PlayVideo1 = findViewById(R.id.PlayVideo1);
+
+
+        initHomePage();
+        initChatBot();
+        initProfilePage();
+        initVideoPage();
+
+        YouTubePlayerView ytPlayer = (YouTubePlayerView)findViewById(R.id.videoPlayer);
+
+        ytPlayer.initialize( api_key,
+                new YouTubePlayer.OnInitializedListener() {
+
+                    @Override
+                    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b)
+                    {
+                        youTubePlayer.loadVideo("bHpPQcCiKJ8", 0);
+                        //youTubePlayer.play();
+                    }
+
+                    // Inside onInitializationFailure
+                    // implement the failure functionality
+                    // Here we will show toast
+                    @Override
+                    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult)
+                    {
+                        Toast.makeText(getApplicationContext(), "Video player Failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_video_player);
+
+        // Get reference to the view of Video player
+        //VideoView ytPlayer = (VideoView)findViewById(R.id.discoveryVideo);
+
+        /*ytPlayer.initialize( api_key,
                 new YouTubePlayer.OnInitializedListener() {
                     // Implement two methods by clicking on red
                     // error bulb inside onInitializationSuccess
@@ -44,11 +83,9 @@ public class MainActivity extends AppCompatActivity {
                     // also handle the play and pause
                     // functionality
                     @Override
-                    public void onInitializationSuccess(
-                            YouTubePlayer.Provider provider,
-                            YouTubePlayer youTubePlayer, boolean b)
+                    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b)
                     {
-                        youTubePlayer.loadVideo("HzeK7g8cD0Y");
+                        youTubePlayer.loadVideo("bHpPQcCiKJ8", 0);
                         youTubePlayer.play();
                     }
 
@@ -56,26 +93,16 @@ public class MainActivity extends AppCompatActivity {
                     // implement the failure functionality
                     // Here we will show toast
                     @Override
-                    public void onInitializationFailure(YouTubePlayer.Provider provider,
-                                                        YouTubeInitializationResult
-                                                                youTubeInitializationResult)
+                    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult)
                     {
                         Toast.makeText(getApplicationContext(), "Video player Failed", Toast.LENGTH_SHORT).show();
                     }
-                });
+                }); */
 
-        homePage = findViewById(R.id.homePage);
-        profilePage = findViewById(R.id.profilePage);
-        chatBot = findViewById(R.id.chatBot);
-
-        initHomePage();
-        initChatBot();
-        initProfilePage();
-
-        Uri uri = Uri.parse("https://www.youtube.com/watch?v=tYS-PSkzi7c");
-        VideoView discoveryVideo = (VideoView) findViewById(R.id.discoveryVideo);
-        discoveryVideo.setVideoURI(uri);
-        discoveryVideo.start();
+        //Uri uri = Uri.parse("https://www.youtube.com/watch?v=tYS-PSkzi7c");
+        //VideoView discoveryVideo = (VideoView) findViewById(R.id.discoveryVideo);
+        //discoveryVideo.setVideoURI(uri);
+        //discoveryVideo.start();
     }
 
 
@@ -103,6 +130,16 @@ public class MainActivity extends AppCompatActivity {
         chatBot.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, ChatbotPage.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initVideoPage() {
+        PlayVideo1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, VideoPlayerActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
             }
