b5743dc6e1747b5542e9dcb118b88c97491b6bae
kedcs22 <kedcs22@gmail.com>
2023-02-22 15:29:07 -0500

    Added User Profile Page View

--- app/src/main/java/com/example/haircareapp/UserProfile.java
+++ app/src/main/java/com/example/haircareapp/UserProfile.java
@@ -23,21 +23,6 @@ public class UserProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
-        savedVideos = (Button) findViewById(R.id.button);
-        savedArticles = findViewById(R.id.button2);
-        followedCreators = findViewById(R.id.button3);
-
-        savedVideos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                opensavedFollow();
-            }
-        });
-
-        public void openactivity_user_profile(){
-            Intent intent = new Intent
-        }
-
         homePage = findViewById(R.id.homePage);
         profilePage = findViewById(R.id.profilePage);
         chatBot = findViewById(R.id.chatBot);
@@ -45,8 +30,47 @@ public class UserProfile extends AppCompatActivity {
         initProfilePage();
         initHomePage();
         initChatBot();
+
+        savedVideos = (Button) findViewById(R.id.button);
+        savedArticles = findViewById(R.id.button2);
+        followedCreators = findViewById(R.id.button3);
+
+        initSavedArticles();
+        initSavedVideos();
+        initFollowedCreators();
+
     }
 
+
+    private void initSavedArticles () {
+        homePage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(UserProfile.this, SavedVideos.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initSavedVideos () {
+        homePage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(UserProfile.this, SavedVideos.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initFollowedCreators() {
+        homePage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(UserProfile.this, FollowedCreators.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
 
     private void initHomePage() {
         homePage.setOnClickListener(new View.OnClickListener() {
